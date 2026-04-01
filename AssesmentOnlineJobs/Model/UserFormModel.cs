be96d79f28d2aafb8562c9f71360a84828e9cbb5
MjBustria <peemacz24@gmail.com>
2026-04-02 06:55:31 +0800

    init

---
+++ AssesmentOnlineJobs/Model/UserFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssesmentOnlineJobs.Model;
+
+public class UserFormModel
+{
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Address is required")]
+    public string Address { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Age is required")]
+    [Range(18, 120, ErrorMessage = "You must be at least 18 years old.")]
+    public int? Age { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    public string Email { get; set; } = string.Empty;
+}
